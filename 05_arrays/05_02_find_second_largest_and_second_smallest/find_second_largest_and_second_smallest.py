00082c37e9589c45f471689712008f2a57427350
saurabhthecodewizard <skmahajan4998@gmail.com>
2024-01-07 22:54:49 +0530

    05_02_find_second_largest_and_second_smallest

---
+++ 05_arrays/05_02_find_second_largest_and_second_smallest/find_second_largest_and_second_smallest.py
@@ -0,0 +1,37 @@
+def secondSmallest(arr, n):
+    if (n < 2):
+        return -1
+    small = float('inf')
+    second_small = float('inf')
+    for i in range(n):
+        if (arr[i] < small):
+            second_small = small
+            small = arr[i]
+        elif (arr[i] < second_small and arr[i] != small):
+            second_small = arr[i]
+    return second_small
+
+def secondLargest(arr, n):
+    if (n < 2):
+        return -1
+    large = float('-inf')
+    second_large = float('-inf')
+    for i in range(n):
+        if (arr[i] > large):
+            second_large = large
+            large = arr[i]
+        elif (arr[i] > second_large and arr[i] != large):
+            second_large = arr[i]
+    return second_large
+
+
+arr = [1, 2, 4, 7, 7, 5]
+n = len(arr)
+
+print("Elements:-")
+print(arr)
+
+sS = secondSmallest(arr, n)
+sL = secondLargest(arr, n)
+print("Second smallest is", sS)
+print("Second largest is", sL)
